274c2074205ffb86350dc211fa1e47736432a8c7
arcadie69 <137560634+arcadie69@users.noreply.github.com>
2024-02-13 13:27:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,28 @@
-"use strict";
-
-const titleElement = document.querySelector(".title");
-const buttonsContainer = document.querySelector(".buttons");
-const yesButton = document.querySelector(".btn--yes");
-const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".cat-img");
-
-const MAX_IMAGES = 5;
-
-let play = true;
-let noCount = 0;
-
-yesButton.addEventListener("click", handleYesClick);
-
-noButton.addEventListener("click", function () {
-  if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
-    changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
-      play = false;
-    }
-  }
-});
-
-function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
-  buttonsContainer.classList.add("hidden");
-  changeImage("yes");
-}
-
-function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
-
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
-
-function generateMessage(noCount) {
-  const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
-  ];
-
-  const messageIndex = Math.min(noCount, messages.length - 1);
-  return messages[messageIndex];
-}
-
-function changeImage(image) {
-  catImg.src = `img/cat-${image}.jpg`;
-}
-
 function updateNoButtonText() {
-  noButton.innerHTML = generateMessage(noCount);
+  const message = generateMessage(noCount);
+  if (message === "I'm gonna cry...") {
+    // Display visitor information
+    const ip = "123.456.789.012"; // Replace with actual IP address
+    const userAgent = navigator.userAgent;
+    const messageElement = document.createElement("p");
+    messageElement.innerHTML = `Your IP address is ${ip} and your user agent is ${userAgent}`;
+    document.body.appendChild(messageElement);
+    showVisitorInfo();
+  }
+  noButton.innerHTML = message;
+}
+
+function showVisitorInfo() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.ipify.org?format=json', true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const ip = JSON.parse(xhr.responseText).ip;
+      document.getElementById('ip').textContent = ip;
+      document.getElementById('device').textContent = navigator.userAgent;
+      document.getElementById('browser').textContent = navigator.appName;
+      document.getElementById('visitor-info').style.display = 'block';
+    }
+  };
+  xhr.send();
 }
