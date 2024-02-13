c87463641009014891f8ce73a4931eb21f7cdf8f
arcadie69 <137560634+arcadie69@users.noreply.github.com>
2024-02-13 14:18:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,22 @@
-<script type="module">
-  "use strict";
+// Retrieve user's IP address using an external API
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ipAddress = data.ip;
+    // Retrieve user's device and browser information
+    const userAgent = navigator.userAgent;
+    const deviceInfo = {
+      platform: navigator.platform,
+      userAgent: userAgent
+    };
 
-  const titleElement = document.querySelector(".title");
-  const buttonsContainer = document.querySelector(".buttons");
-  const yesButton = document.querySelector(".btn--yes");
-  const noButton = document.querySelector(".btn--no");
-  const catImg = document.querySelector(".cat-img");
-  const visitorInfoElement = document.querySelector(".visitor-info");
-
-  const MAX_IMAGES = 5;
-
-  let play = true;
-  let noCount = 0;
-
-  yesButton.addEventListener("click", handleYesClick);
-
-  noButton.addEventListener("click", function () {
-    if (play) {
-      noCount++;
-      const imageIndex = Math.min(noCount, MAX_IMAGES);
-      changeImage(imageIndex);
-      resizeYesButton();
-      updateNoButtonText();
-      if (noCount === MAX_IMAGES) {
-        play = false;
-        fetchIPInfo();
-      }
-    }
+    // Add event listener to the "No" button
+    const noButton = document.querySelector('.btn--no');
+    noButton.addEventListener('click', () => {
+      // Display the retrieved information
+      alert(`IP Address: ${ipAddress}\nDevice Information:\n${JSON.stringify(deviceInfo, null, 2)}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error retrieving IP address:', error);
   });
-
-  function handleYesClick() {
-    titleElement.innerHTML = "Yayyy!! :3";
-    buttonsContainer.classList.add("hidden");
-    changeImage("yes");
-  }
-
-  function resizeYesButton() {
-    const computedStyle = window.getComputedStyle(yesButton);
-    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-    const newFontSize = fontSize * 1.6;
-
-    yesButton.style.fontSize = `${newFontSize}px`;
-  }
-
-  function generateMessage(noCount) {
-    const messages = [
-      "No",
-      "Are you sure?",
-      "Pookie please",
-      "Don't do this to me :(",
-      "You're breaking my heart",
-      "I'm gonna cry...",
-    ];
-
-    const messageIndex = Math.min(noCount, messages.length - 1);
-    return messages[messageIndex];
-  }
-
-  function changeImage(image) {
-    catImg.src = `img/cat-${image}.jpg`;
-  }
-
-  function updateNoButtonText() {
-    noButton.innerHTML = generateMessage(noCount);
-
-    if (noCount === 5) {
-      // Fetch IP information and display it
-      fetchIPInfo();
-
-      // Change the content of the title element
-      titleElement.innerHTML = "I'm gonna cry...";
-
-      // Show the visitor's information
-      visitorInfoElement.classList.remove("hidden");
-
-      // Hide the buttons container
-      buttonsContainer
